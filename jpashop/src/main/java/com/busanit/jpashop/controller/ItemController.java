75e02eed7ae46bacfd26c0b708a1f4f4cd245aab
Gvl21 <thornyk1@gmail.com>
2023-10-30 15:28:43 +0900

    상품 관리 페이지 QueryDSL 동적 쿼리 준비, 인터페이스 작성

--- jpashop/src/main/java/com/busanit/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/com/busanit/jpashop/controller/ItemController.java
@@ -2,11 +2,14 @@ package com.busanit.jpashop.controller;
 
 import com.busanit.jpashop.dto.ItemDto;
 import com.busanit.jpashop.dto.ItemFormDto;
+import com.busanit.jpashop.dto.ItemSearchDto;
 import com.busanit.jpashop.entity.Item;
 import com.busanit.jpashop.service.ItemService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -40,7 +43,7 @@ public class ItemController {
             return "/item/itemForm";
         }
         // 상품 이미지 파일 1개를 필수적으로 넣는 유효성 검증 추가
-        if (itemImgFileList.get(0).isEmpty()){
+        if (itemImgFileList.get(0).isEmpty()) {
             model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 사항입니다.");
             return "/item/itemForm";
         }
@@ -53,23 +56,24 @@ public class ItemController {
     // 아이템 수정 Get 요청
     @GetMapping("/admin/item/{itemId}")
     public String itemDtl(@PathVariable("itemId") Long itemId, Model model) {
-        
+
         try {
             // 아이템 서비스에 위임하여 dto 리턴
             ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
             // 엔티티가 존재할 경우, dto를 모델에 담아 뷰로 전달
             model.addAttribute("itemFormDto", itemFormDto);
             // 경로변수(itemId)에 해당하는 엔티티가 없을 경우
-        } catch(EntityNotFoundException e) {
+        } catch (EntityNotFoundException e) {
             // 상품등록 페이지로 리다이렉트
             return "redirect:/admin/item/new";
         }
 
         return "item/itemForm";
     }
+
     @PostMapping("/admin/item/{itemId}")
-    public String itemUpdate(@Valid ItemFormDto itemFormDto, 
-                             BindingResult bindingResult, 
+    public String itemUpdate(@Valid ItemFormDto itemFormDto,
+                             BindingResult bindingResult,
                              @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList,
                              Model model) {
         // 유효성 검증 : 통과하지 못한 경우 폼으로
@@ -77,7 +81,7 @@ public class ItemController {
             return "/item/itemForm";
         }
         // 상품 이미지 파일 1개를 필수적으로 넣는 유효성 검증 추가
-        if (itemImgFileList.get(0).isEmpty()){
+        if (itemImgFileList.get(0).isEmpty()) {
             model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 사항입니다.");
             return "/item/itemForm";
         }
@@ -86,13 +90,16 @@ public class ItemController {
         return "redirect:/";
     }
 
-@GetMapping("/admin/items")
-public String itemManage(Model model) {
-    // 서비스 계층에서 item 가져오기
-    List<Item> items = itemService.getItemList();
-    model.addAttribute("items", items);
-    return "item/itemMng";
-}
+    @GetMapping("/admin/items")
+    public String itemManage(Model model, ItemSearchDto itemSearchDto) {
+        Pageable pageable = PageRequest.of(0, 3);
+        // 서비스 계층에서 item 페이지 가져오기
+        itemService.getAdminItemPage(itemSearchDto, pageable);
+        // 서비스 계층에서 item 가져오기
+        List<Item> items = itemService.getItemList();
+        model.addAttribute("items", items);
+        return "item/itemMng";
+    }
 
 
 }
---
+++ jpashop/src/main/java/com/busanit/jpashop/repository/ItemRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.busanit.jpashop.repository;
+
+import com.busanit.jpashop.dto.ItemSearchDto;
+import com.busanit.jpashop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ItemRepositoryCustom {
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+
+}
